4c8236d4a45d919271a92afe9d8533a3c503b92c
Kyle Leitham <kyle.leitham@gmail.com>
2025-04-11 17:18:52 -0400

    valid palindrome

---
+++ arrays-and-hashing/valid-palindrome.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+  s = s.replace(/[^0-9a-z]/gi, '').toLowerCase()
+
+  let i = 0
+  let j = s.length - 1
+  while (i < j) {
+    if (s[i] !== s[j]) {
+      return false
+    }
+
+    i++
+    j--
+  }
+
+  return true
+}
+
+module.exports = isPalindrome
---
+++ test/arrays-and-hashing/valid-palindrome.test.js
@@ -0,0 +1,13 @@
+const isPalindrome = require('../../arrays-and-hashing/valid-palindrome')
+
+test('case 1', () => {
+  expect(isPalindrome('A man, a plan, a canal: Panama')).toBe(true)
+})
+
+test('case 2', () => {
+  expect(isPalindrome('race a car')).toBe(false)
+})
+
+test('case 3', () => {
+  expect(isPalindrome(' ')).toBe(true)
+})
